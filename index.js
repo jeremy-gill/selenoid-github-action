656a73622578f7d558a61f32056592e51e3ce105
Anton Ostrenko <Anton_Ostrenko@epam.com>
2020-11-26 23:31:34 +0300

    deleted commented code

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const { execSync } = require("child_process");
 const core = require('@actions/core')
 
-// console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
-
-// execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start`)
-
-// console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
-
 run()
 
 async function run() {
