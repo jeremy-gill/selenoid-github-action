6ee53f50634eeebb17eaab50e8e8226f03ad167f
Anton Ostrenko <Anton_Ostrenko@epam.com>
2020-11-26 23:22:25 +0300

    test selenoid job

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ run()
 async function run() {
     try {
         const args = (core.getInput('args')) ? `--args ${core.getInput('args')}` : ''
+
         const browsers = core.getInput('browsers')
-        const versions = core.getInput('last-versions')
+        if (!browsers) throw new Error('browsers param is required')
+
+        const lastVersions = core.getInput('last-versions')
+        if (!lastVersions) throw new Error('last-versions param is required')
 
         console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
 
-        execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start ${args} --browsers ${browsers} --last-versions ${versions}`)
+        execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start ${args} --browsers ${browsers} --last-versions ${lastVersions}`)
 
         console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
 
