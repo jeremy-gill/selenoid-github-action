176c4b10f016abe2dc435803da1079cf7180474a
Anton Ostrenko <Anton_Ostrenko@epam.com>
2020-11-26 22:17:56 +0300

    added params to run command

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const { execSync } = require("child_process");
+const core = require('@actions/core')
 
-console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
+// console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
 
-execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start`)
+// execSync(`curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid start`)
 
-console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
+// console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
+
+run()
+
+async function run() {
+    try {
+        const limit = core.getInput('limit')
+        const browsers = core.getInput('browsers')
+        const versions = core.getInput('versions')
+
+        console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
+
+        execSync(`curl -s https://aerokube.com/cm/bash | 
+        bash && ./cm selenoid start --args ${limit} --browsers ${browsers} --last-versions ${versions}`)
+
+        console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
+
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
