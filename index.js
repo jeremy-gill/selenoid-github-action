f9d11f842782ba320361f60a329af2a1a3d62686
n-ton <a.ostrenko@gmail.com>
2021-03-24 16:18:47 +0300

    added selenoid version

--- index.js
+++ index.js
@@ -16,11 +16,9 @@ async function run() {
         const lastVersions = core.getInput('last-versions')
         if (!lastVersions) throw new Error('last-versions param is required')
 
-        console.log(`## DOWNLOADING CM AND STARTING SELENOID`);
         const command = `curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid download --version ${version} --force && ./cm selenoid start ${args} --browsers '${browsers}' --last-versions ${lastVersions}`
 
         execSync(command, {stdio: 'inherit'})
-        console.log(`## DOWNLOADING CM AND STARTING SELENOID FINISHED`);
 
         console.log('## CHECKING STATUS')
         execSync('./cm selenoid status', {stdio: 'inherit'})
