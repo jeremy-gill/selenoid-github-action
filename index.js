76714b407dd91ee6971e28f99d6a521664e418a4
Jeremy Gill <jgill@parallax-innovations.com>
2022-03-29 13:43:41 -0400

    ENH: Bring in some capability from Xotabu4/selenoid-github-action
    Extend selenoid command options

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ run()
 async function run() {
     try {
         const args = (core.getInput('args')) ? `--args "${core.getInput('args')}"` : ''
+        const selenoidStartArgs = core.getInput('selenoid-start-arguments')
 
         const version = core.getInput('version')
         if (!version) throw new Error('version param is required')
@@ -16,7 +17,7 @@ async function run() {
         const lastVersions = core.getInput('last-versions')
         if (!lastVersions) throw new Error('last-versions param is required')
 
-        const command = `curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid download --version ${version} --force && ./cm selenoid start ${args} --browsers '${browsers}' --last-versions ${lastVersions}`
+        const command = `curl -s https://aerokube.com/cm/bash | bash && ./cm selenoid download --version ${version} --force && ./cm selenoid start ${selenoidStartArgs} ${args} --browsers '${browsers}' --last-versions ${lastVersions}`
 
         execSync(command, {stdio: 'inherit'})
 
